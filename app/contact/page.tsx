9ffe2c4f5a9bbcad03eb9819059ebad3686e799f
Vineet Katiyar <vineetkatiyar98@gmail.com>
2025-03-10 14:27:25 +0530

    change in home page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,10 +2,9 @@ import ContactForm from '@/components/contact-form'
 
 export default function Contact() {
   return (
-    <section className='pb-24 pt-40'>
+    <section className='pb-24'>
       <div className='container max-w-3xl'>
-        <h2 className='title'>Let&apos;s talk about your project</h2>
-
+        <h2 className='title'>Contact me!</h2>
         <ContactForm />
       </div>
     </section>
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
 import Intro from '@/components/intro'
 import RecentPosts from '@/components/recent-posts'
 import RecentProjects from '@/components/recent-projects'
+import Contact from '@/app/contact/page'
 
 export default function Home() {
   return (
     <section className='pb-24 pt-40'>
       <div className='container max-w-3xl'>
         <Intro />
-
         <RecentPosts />
         <RecentProjects />
+        <Contact />
       </div>
     </section>
   )
