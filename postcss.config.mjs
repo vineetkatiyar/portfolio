50e91da411f91857740b02c0dd5212403b5108d9
Vineet Katiyar <vineetkatiyar98@gmail.com>
2025-09-10 10:43:32 +0530

    style access

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,4 +1,3 @@
-// postcss.config.mjs
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,12 +3,11 @@ import type { Config } from 'tailwindcss'
 
 const config = {
   darkMode: 'class',
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}'
-  ],
+ content: [
+  './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+  './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+  './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+],
   prefix: '',
   theme: {
     container: {
