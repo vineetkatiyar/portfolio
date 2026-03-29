fe8cd2c85c6c8abe85f7a409cf52ed13d045d048
Vineet Katiyar <vineetkatiyar98@gmail.com>
2026-03-29 14:13:47 +0530

    changing intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -8,13 +8,18 @@ export default function Intro() {
   return (
     <section className='flex flex-col-reverse items-start gap-x-10 gap-y-4 pb-24 md:flex-row md:items-center'>
       <div className='mt-2 flex-1 space-y-3 md:mt-0'>
-        <h1 className='title no-underline text-2xl md:text-3xl font-bold'>Hey, I&#39;m Vineet Katiyar.</h1>
+        <h1 className='title text-2xl font-bold no-underline md:text-3xl'>
+          Hey, I&#39;m Vineet Katiyar.
+        </h1>
         <p className='mt-3 font-light text-muted-foreground'>
-          I&#39;m a fullstack developer at Wire Wings in Lucknow <br />
-          I specialize in creating full-stack solutions that solve real-world
-          problems. With hands-on experience in React.js, Next.js, Node.js, and
-          more, I love experimenting with new technologies and sharing my
-          learning experiences with the developer community.
+          I’m a Full Stack Developer at Blox, working on a Bahrain government
+          forensic lab project. <br />
+          I build scalable, production-ready applications using Next.js, React,
+          and Node.js, focusing on performance, clean architecture, and robust
+          API systems. <br />
+          My work includes implementing role-based access control, multilingual
+          platforms (English/Arabic), and integrating serverless backend
+          services like AWS Lambda.
         </p>
       </div>
       <div className='relative'>
